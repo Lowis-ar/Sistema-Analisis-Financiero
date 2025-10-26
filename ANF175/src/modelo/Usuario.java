8e905a77bbdbf8d320c6e7cf977927ae727e8878
JesusMayora <vm22024@ues.edu.sv>
2025-10-26 17:41:59 -0600

    prueba

---
+++ ANF175/src/modelo/Usuario.java
@@ -0,0 +1,12 @@
+package modelo;
+
+public class Usuario {
+
+    int id_usuario;
+    String nombre_usuario;
+    String contraseña;
+    String nivel_acceso; // Administrador, Asesor, Cajero, Consulta
+    String estado;       // Activo, Inactivo 
+    
+    
+}
